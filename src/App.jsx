7b4595441d4ed91fa210fdcb7357994742014dbf
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-03-06 09:51:15 -0300

    Finalizando tela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+//Importando componentes
+import StartScreen from './components/StartScreen'
 
 import './App.css'
 
 function App() {
     return (
         <div className='App'>
-            <h1>Secret Word</h1>
+            <StartScreen />
         </div>
     )
 }
