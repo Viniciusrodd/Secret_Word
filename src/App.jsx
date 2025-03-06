30bb06a70dc54fd10ef6ac626c9dd5d807007ffd
Vinicius Rodrigues <147623457+Viniciusrodd@users.noreply.github.com>
2025-03-06 10:07:20 -0300

    Criando componentes do game

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-//Importando componentes
-import StartScreen from './components/StartScreen'
+// IMPORTANDO HOOKS
+import { useCallback, useEffect, useState } from 'react'
 
+// IMPORTANDO COMPONENTES
+import StartScreen from './components/StartScreen'
+import Game from './components/Game'
+import GameOver from './components/GameOver'
+
+// IMPORTANDO ESTILO
 import './App.css'
 
+// IMPORTANDO DADOS
+import { wordsList } from './data/word'
+
+const stages = [    // ESTÁGIOS DO GAME
+    { id: 1, name: 'start' },
+    { id: 2, name: 'game' },
+    { id: 3, name: 'end' }
+];
+
 function App() {
+    const [ gameStage, SetGameStage ] = useState(stages[0].name);
+    const [ words ] = useState(wordsList);
+
+    console.log(words)
+
     return (
         <div className='App'>
-            <StartScreen />
+            {/* IF's... */}
+            { gameStage === 'start' && <StartScreen /> }
+            { gameStage === 'game' && <Game /> }
+            { gameStage === 'end' && <GameOver /> }
         </div>
     )
 }
